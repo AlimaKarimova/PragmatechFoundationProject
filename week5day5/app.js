3d8580e2e23758c8b45c6aae25d0bea218a4420d
AlimaKarimova <way.s@inbox.ru>
2021-10-24 20:20:12 +0400

    tapsiriq(yarimciq)

---
+++ week5day5/app.js
@@ -0,0 +1,27 @@
+let userName = prompt('Adınızı daxil edin', '');
+
+
+let age = prompt('Yaşınızı daxil edin', '');
+if (age<30) {
+   alert(age**2)
+   
+   
+} 
+else if (age<0) {
+   alert('Düzgün məlumat daxil etməmisiniz')
+   
+}
+else if (age>100) {
+   alert('Düzgün məlumat daxil etməmisiniz')
+   
+}
+else if (30<=age) {
+   alert(9)
+   
+}
+else if (age<=40) {
+   alert(9)
+   
+}
+
+
