f298955a7c0fb09528e09e1c418e7052ea2d6804
AlimaKarimova <way.s@inbox.ru>
2021-10-25 22:08:24 +0400

    tapsiriq tamamlandi

--- BackEnd/javascript tapsiriq/app.js
+++ BackEnd/javascript tapsiriq/app.js
@@ -1,2 +1,10 @@
-let userName = prompt("adınızı daxil edin", 'adınızı daxil edin');
-let userSurname = prompt('soyadınızı daxil edin', 'soyadınızı daxil edin');
+let userName = prompt("adınızı daxil edin")
+for (let i = 0; i < 2; i++)
+console.log(userName);
+
+let userSurname = prompt('soyadınızı daxil edin');
+for (let i = 0; i < 4; i++)
+console.log(userSurname);
+console.log(userName+'|'+'|'+'|'+userSurname);
+console.log('Sizin adınız:'+ userName);
+console.log('Sizin adınız:'+ userSurname);
