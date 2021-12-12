8fd6aeece1ba425ff6bfd3bcf327a5c7958eb414
AlimaKarimova <way.s@inbox.ru>
2021-12-12 23:13:17 +0400

    tapsiriqlarin bir hissesi

---
+++ Algorithms/9dekabr/index.js
@@ -0,0 +1,11 @@
+function printSquareNumber(a) {
+   return a**2
+}
+console.log(printSquareNumber(10))
+
+function printUpperCase(lowerString) {
+   return lowerString.toUpperCase();
+}
+console.log(printUpperCase('salam sen necesen'))
+
+function printSentenceLength(a) 
