93f486cb82651ce7e3b96a5ced54924deddb8c15
AlimaKarimova <way.s@inbox.ru>
2021-12-19 00:41:59 +0400

    tapsiriqlar

---
+++ Algorithms/18dekabr/app.js
@@ -0,0 +1,23 @@
+
+function changeBg(){
+ document.body.style.background = 'red'  
+}
+
+
+function changeTitleColor(){
+   document.querySelector('h1').style.color = 'green'
+} 
+
+
+function changeTextColor(){
+   document.querySelector('ul').style.color = 'yellow'
+}
+
+function cerciveElaveEt(){
+document.querySelector('img').style.border= 'thick solid yellow'
+}
+
+function sekliDeyis(){
+   document.getElementById('image').src= '/Algorithms/16dekabr/images/images(5).jpeg'
+}
+
