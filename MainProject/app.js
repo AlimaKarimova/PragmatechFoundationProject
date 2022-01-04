27d0d8ef20b3659040222111ceb0b7854ccd1114
AlimaKarimova <way.s@inbox.ru>
2022-01-04 22:42:42 +0400

    profila duzelisler ve header js tetbiiqi

---
+++ MainProject/app.js
@@ -0,0 +1,8 @@
+let sectionHeight = document.querySelector('.header-nav-box').clientHeight
+
+function changeNav() {
+    if (window.scrollY > sectionHeight) {
+       document.querySelector('.header-nav-box').className="bg-white"
+    } else{
+    }
+} 
