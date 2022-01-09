34c59b6cde9aaa447c6a5216a479a088a9ae689d
AlimaKarimova <way.s@inbox.ru>
2022-01-09 23:29:29 +0400

    papka adinin deyisdirilmesi

--- FrontEndProject/app.js
+++ FrontEndProject/app.js
@@ -1,8 +1,8 @@
 
+
 let sectionHeight = document.querySelector('.header-nav-box').clientHeight
 
 function changeNav() {
     if (window.scrollY > sectionHeight) {
-       document.querySelector('.header-nav-box').className="bg-white"
-    }
+       document.querySelector('.header-nav-box').className="bg-white"}
 } 
