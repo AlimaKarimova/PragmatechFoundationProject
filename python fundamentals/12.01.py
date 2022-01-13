76fcb5783f9fbbfce627f3b9ce93b59ffeb49a41
AlimaKarimova <way.s@inbox.ru>
2022-01-13 17:51:13 +0400

    tapsiriqlarin bir hissesi

---
+++ python fundamentals/12.01.py
@@ -0,0 +1,53 @@
+
+
+# 1. Kvadrat yaratmaq olarsa daxil edilən 4 ədəddən, ekrana kvadratın sahəsini, olmazsa, həmin ədədlərin cəmini verin.
+# a=input('a ededini daxil edin\n')
+# b=input('b ededini daxil edin\n')
+# c=input('c ededini daxil edin\n')
+# d=input('d ededini daxil edin\n')
+
+# if a==b==c==d:
+#    print(pow(int(a),2))
+# else:
+#    print(int(a)+int(b)+int(c)+int(d))
+
+# 2. Dörd ədəd daxil edin. Onlardan ən böyüyünü çapa verin.
+
+# a=input('a ededini daxil edin\n')
+# b=input('b ededini daxil edin\n')
+# c=input('c ededini daxil edin\n')
+# d=input('d ededini daxil edin\n')
+
+# print(max(int(a), int(b),int(c),int(d)))
+
+# 3. Proqram run olunanda, istifadəçiyə meyvələr menyusu təqdim olunsun. Userdən menyunuzdakı meyvələrdən birinin adını daxil etməsini tələb edin və ekrana meyvənin qiymətini yazdırın. (İstədiyiniz qiyməti yazdırın). Əgər menyuda olmayan meyvə daxil edilsə, ekrana error mesajı verin.
+
+
+# meyveler_menyusu=('menu:\nalma\narmud\nananas\nciyelek')
+# usere_sual=input(meyveler_menyusu+' \n'+'istediyiniz meyve adini yazin\n')
+
+# if usere_sual=='alma':
+#    print('1 manat')
+# elif usere_sual=='armud':
+#    print('2 manat')
+# elif usere_sual=='ananas':
+#    print('4 manat')
+# elif usere_sual=='ciyelek':
+#    print('5 manat')
+# else:
+#    print('anbarda bu meyve yoxdur')
+
+# 4.Qeydiyyat formu düzəldin. İstifadəçi adını daxil etsin. Adın uzunlu 3-dən kiçik 11-dən böyük ola bilməz.Əgər adını düzgün daxil edərsə, soyadının daxil etməsini istəyin. Soyad 5 hərfdən kiçik, 15 hərfdən uzun olmasın. Əgər soyad düzgün daxil edilsə, Daha sonra doğulduğu ili daxil etsin. Doğum ilinin uzunluğu 4 simvoldan ibarət olmalıdır. Sonra email daxil etməsini tələb edin. Emailin uzunluğu 10 hərifdən qısa 22 hərfdən uzzun olmasın, tərkibində @gmail.com olsun və bu hissə daxil etdiyi emailin sonunda olsun. Ardınca parol axil etsin. Parol 6-13 simvol aralığında olmalıdır. Sonra parolu təsdiqləməyini tələb edin. Təsdiqlədiyi parol birinci yazdığı parol ilə eyni olmalıdır. Bütün bunlar düzgün daxil edilərsə, Qeydiyyat tamamlandı mesajı verilsin və istifadəçidən qeydiyyatın detallarına baxmaq istəyib-istəməməsi soruşulsun. Əgər hə desə, aşağıdakı görüntü çapa verilsin: (Qeyd: Yuxarıda sizin verdiyiniz şərtlərə uyğun istifadəçi input daxil etmsəsə, hər verdiyibiz şərtə uyğun error tipli mesaj verin. Məsələn doğum ilini 5 simvoldan ibarət daxil etsə, mesaj verilsin ki 4 simvol daxil edin. Yəni hər şərti müvafiq mesajlar ilə userə izah edin. ============================================= Ad: Murad Soyad: Əliyev Yaş: 22 Email:
+
+user_name=input('adinizi daxil edin')
+ if len(user_name)>=3 and len(user_name)<=11:
+    user_surname=input('soyadinizi daxil edin')
+user_date_birth=input('dogum ilinizi daxil edin')
+user_email=input('emailinizi daxil edin')
+
+if len(user_name)>=3 and len(user_name)<=11:
+  print(user_surname)
+if len(user_surname)>=5 and len(user_name)<=15:
+       print('her sey duzdu')
+
+   
