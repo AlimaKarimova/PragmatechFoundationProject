2034aa1eb1274b04ca3906299f07ad42b6e3ae1a
AlimaKarimova <way.s@inbox.ru>
2022-01-21 00:02:55 +0400

    bezi tapsiriqlar

--- python fundamentals/14.01.py
+++ python fundamentals/14.01.py
@@ -30,31 +30,37 @@
 # sum_func(my_list)
 
 
-# Sample List : (8, 2, 3, -1, 7) Expected Output : -336
-
 # 4.Write a function called returnDay. This function takes in one parameter ( a number from 1-7) and returns the day of the week ( 1 is Sunday, 2 is Monday etc.). If the number is less than 1 or greater than 7, the function should return None.
-# week_day=['monday', 'tuesday','wednesday','thursday','friday','saturday']
-# number_day=[1,2,3,4,5,6,7]
-# a=input('eded daxil edin:' )
-# def returnDay(a):
-#    if int(a)>=1 and int(a)<=7:
-#       for i in number_day:
-#          if a==i:
-#             for x in week_day:
-#                range(len(week_day))==range(len(number_day))
-#             print(week_day[x])
+from re import X
 
-# returnDay(int(a))         
-# Make a list of five or more usernames, including the name 'admin' . Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the list, and print a greeting to each user: • If the username is 'admin' , print a special greeting, such as Hello admin, would you like to see a status report? • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+week_day=['0','monday', 'tuesday','wednesday','thursday','friday','saturday','sunday']
+number_day=[1,2,3,4,5,6,7]
+a=input('eded daxil edin:' )
+a=int(a)
+def returnDay(a):
+   for i in number_day:
+      if a>1 and a<7 and a==i:
+         number_day.index(i)==range(len(week_day))
+         print(f'{a}-nci gun {week_day[i]}')
+
+returnDay(a)       
+
+# Make a list of five or more usernames, including the name    'admin' . Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the list, and print a greeting to each user: • If the username is 'admin' , print a special greeting, such as Hello admin, would you like to see a status report? • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+# def greeting(*user_Name):
+#    user_Name=input('please,write your nick name')
 
 # dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"} Verilen dictionary-e esasen asgidaki suallari cavablandirmaq ucun ekrana sualin cavabiniz yazdirin. Bunun ucun userden input alin. Eger user “born”, “when” sozlerini daxil etse program texmin etsin ki user ne sorushmaq isteyir. Meselen born ve when sozleri varsa cumlede user cox guman ki “When was Plato born?” sualina cavab axtarir. Proqram o sozleri gorub sorushsun ki, “Maybe did you mean “When was Plato born?” “. Bu suali sorushduqda yes ve no secimleri verin. Eger yes yazsa dictionarydeki datadan istifade ederek Platonun doguldugu ili usere gosterin.Meselen country daxil etse proqram texmin etsin ki user platonun doguldugu yeri axtarir ve siz de ele proqram yazin ki ona uygun cavab qaytarin. Eger mentiqsiz soz daxil edilse not found verin ekrana.
 
-# len() funksiyasini ozunuz yazmaga calishin.....................................................................................................
+# len() funksiyasini ozunuz yazmaga calishin
+
 # my_list= 'hello world'
 # count=0
 # for i in my_list:
 #    count+=1
-print(count)
+# print(count)
+
+
 # funksiya yazin ededlerden ibaret list qebul etsin ve eger listin birinci ve sonuncu elementleri beraberdise return True qaytarsin. Mes: [1,2,3,1] bu halda True qaytaracag
 # number = input('ededleri daxil  edin: ')
 # my_list= number.split()
--- python fundamentals/17.01.py
+++ python fundamentals/17.01.py
@@ -1,6 +1,5 @@
 # 1. Write a program to create a function show_employee() using the following conditions.
 # It should accept the employee’s name and salary and display both. If the salary is missing in the function call then assign default value 9000 to salary
-
 # expected result: showEmployee("Ben", 12000) showEmployee("Jessa") Name: Ben salary: 12000 Name: Jessa salary: 9000
 
 # def show_employee(name,salary=9000):
@@ -10,6 +9,10 @@
 
 # 2.'test' adinda function yaradin, funksiyaya parametrleri key value shekline gonderin istenilen sayda, funksiya gonderilen ededlerden cut olanlarin sayini tapsin
 # expected result: test(a=4, b=1, c=2) result=2
+# yas=input('yasinizi daxil edin')
+# tevellud=input('tevelludunuzu daxil edin')
+# tecrube=input('is tecrubeniz')
+# maas=input('aldiginiz maas')
 # def test(**kwarg):
 #    say=0
 #    x=kwarg.values()
@@ -18,12 +21,14 @@
 #          say+=1
 #    print(say)
 
-# test(yas=25, tevellud=2000,tecrube=2, maas=800)
+# test(yas,tevellud,tecrube,maas)
 
 # 3.math.ceil funksiyasini ozunuz yazin
 
-# 4.stringlerin index metodunu ozunuz yazin
 
+
+
+# 4.stringlerin index metodunu ozunuz yazin
 # def index_metodu(a):
 #    index=-1
 #    for i in a:
