0fe468f04b36fb4efceefd2511bafc4f13925ed8
AlimaKarimova <way.s@inbox.ru>
2022-01-21 00:38:48 +0400

    elave bir tapsiriq da yazdim

--- python fundamentals/14.01.py
+++ python fundamentals/14.01.py
@@ -31,24 +31,33 @@
 
 
 # 4.Write a function called returnDay. This function takes in one parameter ( a number from 1-7) and returns the day of the week ( 1 is Sunday, 2 is Monday etc.). If the number is less than 1 or greater than 7, the function should return None.
-from re import X
 
 
-week_day=['0','monday', 'tuesday','wednesday','thursday','friday','saturday','sunday']
-number_day=[1,2,3,4,5,6,7]
-a=input('eded daxil edin:' )
-a=int(a)
-def returnDay(a):
-   for i in number_day:
-      if a>1 and a<7 and a==i:
-         number_day.index(i)==range(len(week_day))
-         print(f'{a}-nci gun {week_day[i]}')
+# week_day=['0','monday', 'tuesday','wednesday','thursday','friday','saturday','sunday']
+# number_day=[1,2,3,4,5,6,7]
+# a=input('eded daxil edin:' )
+# a=int(a)
+# def returnDay(a):
+#    for i in number_day:
+#       if a>1 and a<7 and a==i:
+#          number_day.index(i)==range(len(week_day))
+#          print(f'{a}-nci gun {week_day[i]}')
 
-returnDay(a)       
+# returnDay(a)       
 
 # Make a list of five or more usernames, including the name    'admin' . Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the list, and print a greeting to each user: • If the username is 'admin' , print a special greeting, such as Hello admin, would you like to see a status report? • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 # def greeting(*user_Name):
 #    user_Name=input('please,write your nick name')
+
+# user_Name=input('please,write your nick name')
+# def my_func(*arg):
+#    if user_Name=='admin':
+#       print('Hello admin, would you like to see a status report?')
+#    else:
+#       print(f'Hello {user_Name}, thank you for logging in again')
+
+# my_func(user_Name)
+
 
 # dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"} Verilen dictionary-e esasen asgidaki suallari cavablandirmaq ucun ekrana sualin cavabiniz yazdirin. Bunun ucun userden input alin. Eger user “born”, “when” sozlerini daxil etse program texmin etsin ki user ne sorushmaq isteyir. Meselen born ve when sozleri varsa cumlede user cox guman ki “When was Plato born?” sualina cavab axtarir. Proqram o sozleri gorub sorushsun ki, “Maybe did you mean “When was Plato born?” “. Bu suali sorushduqda yes ve no secimleri verin. Eger yes yazsa dictionarydeki datadan istifade ederek Platonun doguldugu ili usere gosterin.Meselen country daxil etse proqram texmin etsin ki user platonun doguldugu yeri axtarir ve siz de ele proqram yazin ki ona uygun cavab qaytarin. Eger mentiqsiz soz daxil edilse not found verin ekrana.
 
