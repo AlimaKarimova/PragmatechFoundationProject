b3805ec37ec75b67b74df2b16cc98c135c8e854a
AlimaKarimova <way.s@inbox.ru>
2022-02-04 18:57:02 +0400

    3ve 4 tapsiriqlar yazildi

--- task03/sual4.py
+++ task03/sual4.py
@@ -1,7 +1,16 @@
 
-# 3. Python vasitəsi ilə bir şəkli bir qovluqdan başqa qovluğa kopyalayan app yazaraq linkini qeyd edin
-
 # 4. Masaüstü ya da hər hansı bir yerdə bir fayl mövcuddur. Sizdən tələb olunan bu faylın adını dəyişdirən kod yazaraq linkini qeyd etməyinizdir. İnput vasitəsi ilə sadəcə faylın olduğu folder və faylın adı daxil edilməlidir. Siz bu inputlara əsasən ad dəyişdirmə əməlyatını aparmalısını
 
-import os
+# fayllar uzerinde emeliyyatlar aparmaq ucun shutil ve ya os modullarindan istifade ede bilerik
+# import os
+# kohne_ad= input('kohne adi yazin ')
+# teze_ad= input('teze adi yazin  ')
+# # os.path.exists() metodu ise verilen yolun movcud olub olmadigini yoxlamaq ucundur.
+# if os.path.exists(kohne_ad):
+#    os.rename(kohne_ad,teze_ad)
 
+
+# 3. Python vasitəsi ilə bir şəkli bir qovluqdan başqa qovluğa kopyalayan app yazaraq linkini qeyd edin
+
+# import shutil
+# shutil.copyfile(r'C:\Users\User\Desktop\alime\task03\sekil_papkasi\istockphoto-1186330948-612x612.jpg', r'C:\Users\User\Desktop\alime\task03\teze_sekil_papkasi')
