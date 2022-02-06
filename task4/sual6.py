002bd310e5b1a96031aeda107267fcc8e9e6aac3
AlimaKarimova <way.s@inbox.ru>
2022-02-07 01:53:32 +0400

    decorator tapsirigi yazildi

---
+++ task4/sual6.py
@@ -0,0 +1,18 @@
+#  string return edən metod üçün mətnin uzunluğunu istehsal edən dekorator yazın. Bunu javascript ya da python istifadə edərək yaza bilərsiniz. Kodların olduğu gist linkini qeyd edin.
+
+# def metninUzunlugu(func):
+
+#    def daxili():
+#       netice=func()
+#       uzunluq=len(netice)
+#       return uzunluq
+#    return daxili   
+
+
+
+# @metninUzunlugu
+# def stringreturn():
+#    return 'decorator yazildi'
+
+
+# print(stringreturn())
